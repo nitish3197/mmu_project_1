419123d2e49ac2ee920b34323e9edc1b95b50c6a
nitish3197 <nitish_3197@yahoo.co.in>
2017-08-09 12:12:57 +0530

    new file added.

--- eo.py
+++ eo.py
@@ -1,6 +1,7 @@
 print " Check whether number is even or odd !!"
 x=int(raw_input(" Enter the number : "))
 if x%2==0:
-        print " The Given Number " + "("+ str(x)  + ")" + "is even"
-else:
-        print " The Given Number " + "("+ str(x) + ")" + "is odd"
+        print " The Given Number " + "("+ str(x)  + ")" + "is even "
+else :
+        print " The Given Number " + "("+ str(x)  + ")" + "is even "
+
